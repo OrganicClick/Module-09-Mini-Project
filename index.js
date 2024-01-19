1d3c7b4d2d841955441199682d35b13a5da01df0
Austin Luper <austin.luper@gmail.com>
2024-01-18 21:06:52 -0500

    Added a function that utilizes inquirer to prompt user for information to populate their HTML portfolio page

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ const fs = require('fs');
 
 // Defined import of inquirer npm
 const inquirer = require('inquirer');
+
+// Function that utilizes inquirer to prompt user for information
+// to populate their HTML portfolio page
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'Where are you from?',
+      name: 'location',
+    },
+    {
+      type: 'input',
+      message: 'What is your job?',
+      name: 'job',
+    },
+    {
+        type: 'input',
+        message: 'Please provide your LinkedIn page:',
+        name: 'LinkedIn',
+      },
+      {
+        type: 'input',
+        message: 'Please provide your GitHub page:',
+        name: 'GitHub',
+      },
+  ])
+  .then((response) =>
